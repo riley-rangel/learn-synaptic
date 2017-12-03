9b1ac539dcb8bdb1ac895da062842f095c099dbd
Riley Rangel <rrangel91@gmail.com>
2017-12-02 17:55:47 -0800

    Create a simple neural network. #1

---
+++ network.js
@@ -0,0 +1,41 @@
+const { Network, Layer } = require('synaptic')
+
+const inputLayer = new Layer(2)
+const hiddenLayer = new Layer(3)
+const outputLayer = new Layer(1)
+
+inputLayer.project(hiddenLayer)
+hiddenLayer.project(outputLayer)
+
+const myNetwork = new Network({
+  input: inputLayer,
+  hidden: [ hiddenLayer ],
+  output: outputLayer
+})
+
+const learningRate = 0.3
+
+for (let i = 0; i < 20000; i++) {
+  myNetwork.activate([ 0, 0 ])
+  myNetwork.propagate(learningRate, [ 0 ])
+  myNetwork.activate([ 0, 1 ])
+  myNetwork.propagate(learningRate, [ 1 ])
+  myNetwork.activate([ 1, 0 ])
+  myNetwork.propagate(learningRate, [ 1 ])
+  myNetwork.activate([ 1, 1 ])
+  myNetwork.propagate(learningRate, [ 0 ])
+}
+
+const inputs = [
+  [ 0, 0 ],
+  [ 0, 1 ],
+  [ 1, 0 ],
+  [ 1, 1 ]
+]
+
+inputs.forEach(input => {
+  console.log({
+    input,
+    result: myNetwork.activate(input)
+  })
+})
